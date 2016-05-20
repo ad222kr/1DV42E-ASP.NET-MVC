b0edad4fa419b4511241e44bd6193bc96ff886f3
Alex <ad222kr@student.lnu.se>
2016-05-20 02:56:13 +0200

    Installed EF and created basic POCO-classes

---
+++ EventAppMVC/Models/Attendee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventAppMVC.Models
+{
+    public class Attendee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        
+        public virtual Event Event { get; set; }
+    }
+}
